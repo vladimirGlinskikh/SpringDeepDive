2f5a2373828850800a9a8c6e33e9c47b8c19381d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-10-05 23:16:55 +0300

    Delete Java Objects for Database

--- src/main/java/kz/zhelezyaka/hibernate/entities/Main.java
+++ src/main/java/kz/zhelezyaka/hibernate/entities/Main.java
@@ -4,31 +4,20 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        SessionFactory factory = new Configuration()
+        try (SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
-                .buildSessionFactory();
-        try {
+                .buildSessionFactory()) {
             Session session = factory.getCurrentSession();
-//            Employee employee1 = new Employee("Vladimir", "Glinskikh", "IT");
-//            Employee employee2 = new Employee("Svetlana", "Svetina", "MANAGER");
             session.beginTransaction();
 
-            List<Employee> employees = session.createQuery("from Employee " +
-                            "where firstname = 'Svetlana'")
-                    .getResultList();
-            for (Employee e : employees) {
-                System.out.println(e);
-            }
+            session.createQuery("delete Employee where firstname = 'Vladimir'")
+                    .executeUpdate();
 
             session.getTransaction().commit();
 
-        } finally {
-            factory.close();
         }
     }
 }
