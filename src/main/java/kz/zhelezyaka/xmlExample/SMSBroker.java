1c35ff07e8b25aeddfd771d14b9318561fbdb349
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-11-10 18:36:52 +0300

    Two Phases Constructor in Action

--- src/main/java/kz/zhelezyaka/xmlExample/SMSBroker.java
+++ src/main/java/kz/zhelezyaka/xmlExample/SMSBroker.java
@@ -1,11 +1,27 @@
 package kz.zhelezyaka.xmlExample;
 
+import javax.annotation.PostConstruct;
+
 public class SMSBroker implements Broker {
+    @InjectRandomInt(min = 2, max = 5)
+    private int reiterate;
     private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Start constructor two phase");
+        System.out.println(reiterate);
+    }
+
+    public SMSBroker() {
+        System.out.println("Start constructor one phase");
+    }
 
     @Override
     public void sms() {
-        System.out.println("sms: " + message);
+        for (int i = 0; i < reiterate; i++) {
+            System.out.println("sms: " + message);
+        }
     }
 
     public void setMessage(String message) {
